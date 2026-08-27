ae3c1f496f65dc1c2a1202f9eb789626dcdb882a
Mams62 <86874566+Mams62@users.noreply.github.com>
2026-08-27 02:59:59 +0100

    Add Spring Boot application entry point

---
+++ src/main/java/com/wavetransakt/WaveTransaktApiApplication.java
@@ -0,0 +1,11 @@
+package com.wavetransakt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveTransaktApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WaveTransaktApiApplication.class, args);
+    }
+}
