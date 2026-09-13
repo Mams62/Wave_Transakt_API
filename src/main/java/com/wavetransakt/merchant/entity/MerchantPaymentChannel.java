f9d8f2ae82e5628277fa1cd67b49c572da514b4c
Mams62 <86874566+Mams62@users.noreply.github.com>
2026-09-13 02:22:48 +0100

    Add merchant payment channel enum

---
+++ src/main/java/com/wavetransakt/merchant/entity/MerchantPaymentChannel.java
@@ -0,0 +1,10 @@
+package com.wavetransakt.merchant.entity;
+
+public enum MerchantPaymentChannel {
+    QR,
+    CARD_CONTACTLESS,
+    CARD_CHIP,
+    CARD_MAGSTRIPE,
+    BANK_TRANSFER,
+    WALLET
+}
