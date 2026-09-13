207ae99fd3e03dca231af78de64bc23f4efe7cad
Mams62 <86874566+Mams62@users.noreply.github.com>
2026-09-13 06:33:10 +0100

    Add POS session authentication exception

---
+++ src/main/java/com/wavetransakt/merchant/exception/PosSessionAuthenticationException.java
@@ -0,0 +1,7 @@
+package com.wavetransakt.merchant.exception;
+
+public class PosSessionAuthenticationException extends RuntimeException {
+    public PosSessionAuthenticationException(String message) {
+        super(message);
+    }
+}
